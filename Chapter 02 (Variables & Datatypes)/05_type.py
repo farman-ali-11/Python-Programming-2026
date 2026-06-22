f6d35ec255c7beb7f2eeef0f9453a9ccb8bf97c0
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-06-23 00:05:05 +0500

    Add python code

---
+++ Chapter 02 (Variables & Datatypes)/05_type.py
@@ -0,0 +1,9 @@
+a = 45.333
+b = 45
+c = "Farman"
+t = type(a) # class 'float'
+u = type(float(b)) # class 'float'
+v = type(c) # class 'str'
+print(t)
+print(u)
+print(v)
---
+++ Chapter 02 (Variables & Datatypes)/06_input.py
@@ -0,0 +1,7 @@
+a = int(input("Enter Number 1: "))
+
+b = int(input("Enter Number 2: "))
+
+print("Number 1 is: ", a)
+print("Number 2 is: ", b) 
+print("Sum is: ", a + b)
