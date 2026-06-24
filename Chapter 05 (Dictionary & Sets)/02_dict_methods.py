6562da23720a41a4d0fc6cb597568706ebc03cdc
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-06-24 23:59:28 +0500

    Add python code

---
+++ Chapter 05 (Dictionary & Sets)/02_dict_methods.py
@@ -0,0 +1,18 @@
+marks = {
+
+    "Farman" : 88,
+    "Ahmed" : 50,
+    "Kamran" : 67,
+    20 : "Kashif"
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Farman" : 99, "Nouman" : 98})
+# print(marks)
+
+# print(marks.get("Farman2")) # Prints None
+# print(marks["Farman2"]) # Returns an Error
+
+print(marks.pop("Kamran"))
