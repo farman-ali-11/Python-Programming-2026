4fabab93f0becddfe4076d0c4df7e909bd4958b4
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-06-25 00:16:43 +0500

    Add python code

--- Chapter 05 (Dictionary & Sets)/02_dict_methods.py
+++ Chapter 05 (Dictionary & Sets)/02_dict_methods.py
@@ -3,16 +3,18 @@ marks = {
     "Farman" : 88,
     "Ahmed" : 50,
     "Kamran" : 67,
-    20 : "Kashif"
+    20 : "Kashif",
+    "Rehman" : 45
 }
 
-# print(marks.items())
-# print(marks.keys())
-# print(marks.values())
-# marks.update({"Farman" : 99, "Nouman" : 98})
-# print(marks)
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Farman" : 99, "Nouman" : 98})
+print(marks)
 
-# print(marks.get("Farman2")) # Prints None
-# print(marks["Farman2"]) # Returns an Error
+print(marks.get("Farman2")) # Prints None
+print(marks["Farman2"]) # Returns an Error
 
-print(marks.pop("Kamran"))
+# print(marks.popitem())
+# print(marks.pop("Kamran", 67))
