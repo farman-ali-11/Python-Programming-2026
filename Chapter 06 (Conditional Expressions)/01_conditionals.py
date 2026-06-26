ea6e943eaa839d7a0e2261f47050987bfbb49748
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-06-27 00:55:23 +0500

    Add python code

---
+++ Chapter 06 (Conditional Expressions)/01_conditionals.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your age: "))
+
+# if else statement
+
+if (a>=20):
+    print("You are above the age of consent")
+    print("Good for you")
+
+else:
+    print("You are below the age of consent")
+
+
+print("End of Program")
