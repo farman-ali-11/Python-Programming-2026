d77b817ab75b7f1783bf00c7095e7a8e14cff017
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-06-27 14:35:58 +0500

    Add python code

---
+++ Chapter 06 (Practice)/problem5.py
@@ -0,0 +1,9 @@
+l = ["Farman", "Kamran", "Ahmed", "Kashif"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("Your name is in the list")
+
+else:
+    print("Your name is not in the list")
