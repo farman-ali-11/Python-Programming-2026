51181739b3eedb093a9b6faa80ddda5b094ce608
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-06-30 18:24:32 +0500

    Add python code

---
+++ Chapter 07 (Loops in Python)/06_for_loop_iterate.py
@@ -0,0 +1,19 @@
+## for loop with lists
+l = [23, 33, 22, 566, 866, 9594, 00, 0000, 278]
+
+for i in l:
+    print(i)
+
+
+## for loop with tuple
+t = (2, 4 ,6, 7, 9)
+
+for i in t:
+    print(i)
+
+
+## for loop with strings
+f = "Farman"
+
+for i in f:
+    print(i)
