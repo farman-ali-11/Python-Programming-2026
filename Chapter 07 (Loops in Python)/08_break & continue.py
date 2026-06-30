87d5c1a65f4425f11caf4c31cec7e49a7b8d866a
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-06-30 18:39:01 +0500

    Add python code

---
+++ Chapter 07 (Loops in Python)/08_break & continue.py
@@ -0,0 +1,11 @@
+
+for a in range(2, 30):
+    if(a==20):
+        break  # Exit the loop right now
+    print(a)
+
+
+for b in range(2, 30):
+    if(b==20):
+        continue  # Skip this iteration
+    print(b)
