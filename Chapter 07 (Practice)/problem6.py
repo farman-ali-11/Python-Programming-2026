ab716887d86abb5830ee85abbe8286f7ccae8e38
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-02 13:50:39 +0500

    Add python code

---
+++ Chapter 07 (Practice)/problem6.py
@@ -0,0 +1,11 @@
+# Find factorial of given number
+
+n = int(input("Enter the number: "))
+
+product = 1
+
+for i in range(1, n+1):
+
+    product = product * i
+
+print(f"The factorial of {n} is {product}")
