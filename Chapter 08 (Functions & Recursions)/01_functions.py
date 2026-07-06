84b75a415e5799533df3234f2046ccb5be191dab
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-06 23:56:49 +0500

    Add python code

---
+++ Chapter 08 (Functions & Recursions)/01_functions.py
@@ -0,0 +1,11 @@
+def avg():
+    a = int(input("Enter the number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+
+    avg = (a + b + c)/3
+    print(avg)
+
+avg()
+avg()
+avg()
