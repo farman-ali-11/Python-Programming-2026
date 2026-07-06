e8a02bae4614775c6811a2f3628235f8b729aaaf
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-07 02:14:05 +0500

    Add python code

---
+++ Chapter 08 (Practice)/problem6.py
@@ -0,0 +1,8 @@
+# Convert inches to cm
+
+def inch_to_cms(n):
+    return (n*2.54)
+
+n = int(input("Enter value in Inches: "))
+c = inch_to_cms(n)
+print(f"The corresponding value in cms is: {c} Cm")
