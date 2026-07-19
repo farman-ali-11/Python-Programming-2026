a3041b42453b2c9fb44dab7d017b8a142a94b5ba
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-19 20:51:41 +0500

    Add python code

--- Grand_Practice/program10.py
+++ Grand_Practice/program10.py
@@ -1,10 +1,9 @@
 # Temperature Conversion
 
-
 def tempt(n):
     return (n * 1.8) + 32
 
 
 n = int(input("Enter the temperature in Celsius: "))
-print(tempt(n))
-
+c = round(tempt(n), 2)
+print(f"The temperature in Farenheit is: {c}°F")
