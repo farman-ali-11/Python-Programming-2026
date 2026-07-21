ae4ad5ad6a331cae672d90cf56f9bd51219f2fe3
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-21 22:24:16 +0500

    Add python code

--- Grand_Practice/program11.py
+++ Grand_Practice/program11.py
@@ -5,8 +5,8 @@ for i in range(1, 10):
     print((n*2)+5)
     n += 3
 
-if (i == 13):
-    print(f"The entered value is correct: {i}")
+if (n >= 13):
+    print(f"The entered value is correct: {n}")
 
 else:
     print("Value is not found")
---
+++ Grand_Practice/program12.py
@@ -0,0 +1,8 @@
+
+n = input("Enter the value: ")
+
+if (n == 4):
+    print(f"The value: {n}")
+
+else:
+    print(f"The value is not correct: {n}")
