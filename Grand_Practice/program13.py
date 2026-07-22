92b1069940ee4e3f8b618fe674f4c705ab3bb210
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-22 19:32:00 +0500

    Add python code

--- Grand_Practice/program13.py
+++ Grand_Practice/program13.py
@@ -5,5 +5,8 @@ a = input("Enter your name: ")
 if ( a == "farman"):
     print(f"{a} is a good boy..")
 
+elif (a == "kashif"):
+    print(f"{a} is not a good boy..")
+
 else:
     print(f"{a} is a bad boy... ")
