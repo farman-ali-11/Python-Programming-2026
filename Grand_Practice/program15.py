97aaa0401d67603490b693bbec7f7571a2fd1f43
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-25 19:25:05 +0500

    Add python code

--- Grand_Practice/program15.py
+++ Grand_Practice/program15.py
@@ -1,11 +1,15 @@
+'''
+Sum of n natural number
+      sum(n-1) + n
 
-# sum(n-1) + n
+'''
 
 def sum(n):
-    if (n==0):
-        return
+    if (n==1):
+        return 1
 
-    return sum(n-1)+n
+    return sum(n-1) + n
 
 n = int(input("Enter the value: "))
 b = sum(n)
+print(f"The sum of {n} natural number is: {b}")
