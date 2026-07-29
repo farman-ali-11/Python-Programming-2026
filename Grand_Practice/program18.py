8a54193c8ca32b7e6baecd0b2e045ab7ebf4fdb9
farman-ali-11 <farmanalirajper2001@gmail.com>
2026-07-29 19:24:57 +0500

    Add python code

--- Grand_Practice/program18.py
+++ Grand_Practice/program18.py
@@ -1,3 +1,4 @@
+# Function with arguments
 
 def fr(name, greeting="Best of luck.."):
     print(f"Hello, {name}")
@@ -5,3 +6,9 @@ def fr(name, greeting="Best of luck.."):
 
 fr("Farman Rajper", "You are the best..")
 fr("Ahmed Khan")
+
+
+def rf():
+    print("Good Day")
+
+rf()
